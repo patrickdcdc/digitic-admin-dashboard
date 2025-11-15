e3030d13f8dccaa1e921f69d83c3ddacc47cafa3
Patrick Mutua <patrickmutua719@gmail.com>
2025-11-15 09:40:26 +0300

    Add orders, enquiries, dashboard, bloglist, blogcatlist pages

---
+++ src/pages/Blogcatlist.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Table } from "antd";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+  },
+];
+const data1 = [
+  {
+    key: "1",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    age: 32,
+    address: "Sydney No. 1 Lake Park",
+  },
+  {
+    key: "4",
+    name: "Disabled User",
+    age: 99,
+    address: "Sydney No. 1 Lake Park",
+  },
+];
+// rowSelection object indicates the need for row selection
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(
+      `selectedRowKeys: ${selectedRowKeys}`,
+      "selectedRows: ",
+      selectedRows
+    );
+  },
+  getCheckboxProps: (record) => ({
+    disabled: record.name === "Disabled User", // Column configuration not to be checked
+    name: record.name,
+  }),
+};
+
+const Blogcatlist = () => {
+  return (
+    <div>
+      <h3 className="mb-4">Blog categories</h3>
+      <div>
+        <Table columns={columns} dataSource={data1} />
+      </div>
+    </div>
+  );
+};
+
+export default Blogcatlist;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,160 @@
 import React from "react";
+import { GoArrowDownRight } from "react-icons/go";
+import { BsArrowUpRight } from "react-icons/bs";
+import { Column } from "@ant-design/plots";
+import { Table } from "antd";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+  },
+];
+const data1 = [
+  {
+    key: "1",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    age: 32,
+    address: "Sydney No. 1 Lake Park",
+  },
+  {
+    key: "4",
+    name: "Disabled User",
+    age: 99,
+    address: "Sydney No. 1 Lake Park",
+  },
+];
+// rowSelection object indicates the need for row selection
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(
+      `selectedRowKeys: ${selectedRowKeys}`,
+      "selectedRows: ",
+      selectedRows
+    );
+  },
+  getCheckboxProps: (record) => ({
+    disabled: record.name === "Disabled User", // Column configuration not to be checked
+    name: record.name,
+  }),
+};
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const data = [
+    { type: "Jan", value: 600 },
+    { type: "Feb", value: 125 },
+    { type: "March", value: 240 },
+    { type: "April", value: 190 },
+    { type: "May", value: 220 },
+    { type: "June", value: 500 },
+    { type: "July", value: 100 },
+    { type: "August", value: 150 },
+  ];
+
+  const config = {
+    data,
+    xField: "type",
+    yField: "value",
+    style: {
+      fill: ({ type }) => {
+        if (type === "10-30分" || type === "30+分") {
+          return "#22CBCC";
+        }
+        return "#3363ffff";
+      },
+    },
+    label: {
+      text: (originData) => {
+        const val = parseFloat(originData.value);
+        if (val < 0.05) {
+          return (val * 100).toFixed(1) + "%";
+        }
+        return "";
+      },
+      offset: 10,
+    },
+    legend: false,
+  };
+
+  return (
+    <div>
+      <h3 className="mb-4">Dashboard</h3>
+      <div
+        className="d-flex justify-content-between align-items-center gap-3"
+        style={{ rowGap: "2rem", columnGap: "2rem" }}
+      >
+        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="mb-0">Total</p> <h4 className="mb-0">$1100</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="red">
+              <GoArrowDownRight />
+              32%
+            </h6>
+            <p className="mb-0">Compared to April</p>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="mb-0">Total</p> <h4 className="mb-0">$1100</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="green">
+              <BsArrowUpRight />
+              32%
+            </h6>
+            <p className="mb-0">Compared to April 2022</p>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="mb-0">Total</p> <h4 className="mb-0">$1100</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="green">
+              <BsArrowUpRight />
+              32%
+            </h6>
+            <p className="mb-0">Compared to April 2022</p>
+          </div>
+        </div>
+      </div>
+      <div className="mt-4">
+        <h3 className="mb-4">Income statistics</h3>
+        <div>
+          <Column {...config} />
+        </div>
+      </div>
+      <div className="mt-4">
+        <h3 className="mb-4">Recent orders</h3>
+        <div>
+          <Table columns={columns} dataSource={data1} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
